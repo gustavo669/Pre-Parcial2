1278b7d127b584fa824a398c096faec9085c52f3
Gustavo Adolfo Godoy Barrera <99930726+gustavo669@users.noreply.github.com>
2024-09-13 18:55:43 -0600

    Add files via upload

--- src/main/java/umg/progra2/Conexión/MySQLConnection.java
+++ src/main/java/umg/progra2/Conexión/MySQLConnection.java
@@ -1,2 +1,22 @@
-package umg.progra2.Conexión;public class MySQLConnection {
+package umg.progra2.Conexión;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/db_telebot";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexión exitosa a la base de datos.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
